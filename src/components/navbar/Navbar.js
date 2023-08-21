e5c502ebefb2942972b3ffd6d40142d8700a599c
Leon Parsons <MY_NAME@example.com>
2023-08-21 12:35:31 +1200

    Basic react setup with Tailwind installed

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div className='navBarBody'>
+      <h1>this is navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
